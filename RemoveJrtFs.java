a98adab5e210b2802428281117a076a21947fbec
Michal Sevcik <michal.sevcik@protonmail.com>
2025-02-03 21:06:12 +0100

    Dependency changes from previous version

---
+++ RemoveJrtFs.java
@@ -0,0 +1,6 @@
+import java.nio.file.*;
+public class RemoveJrtFs {
+	public static void main(String[] args) throws Exception {
+		Files.deleteIfExists(Paths.get("/usr/lib/jvm/java-17-openjdk-amd64/lib/jrt-fs.jar"));
+	}
+}
